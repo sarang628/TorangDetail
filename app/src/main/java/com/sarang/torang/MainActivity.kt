ce78efe38289746557b9fd7885ce2ca8459935b6
sryang <sarang628@gmail.com>
2025-06-25 15:29:22 +0800

    update

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -9,12 +9,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import com.sarang.library.RestaurantDetailNavigationScreen
 import com.sarang.torang.compose.restaurant.LocalFeeds
 import com.sarang.torang.compose.restaurant.LocalImageLoader
 import com.sarang.torang.compose.restaurant.LocalPullToRefresh
+import com.sarang.torang.compose.restaurant.LocalRestaurantInfo
+import com.sarang.torang.compose.restaurant.RestaurantInfo
+import com.sarang.torang.data.RestaurantDetail
 import com.sarang.torang.di.image.customImageLoader
 import com.sarang.torang.di.restaurant_detail.customPullToRefresh
 import com.sarang.torang.di.restaurant_detail.provideFeeds
@@ -38,11 +43,20 @@ class MainActivity : ComponentActivity() {
 }
 //@formatter:on
 
+@OptIn(ExperimentalMaterial3Api::class)
+val CustomRestaurantInfo: RestaurantInfo = {
+    RestaurantDetailNavigationScreen(restaurantId = 234)
+}
+
+
 @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
 @Composable
 fun Main() {
-    CompositionLocalProvider(LocalImageLoader provides customImageLoader, LocalPullToRefresh provides customPullToRefresh,
-        LocalFeeds provides provideFeeds
+    CompositionLocalProvider(
+        LocalImageLoader provides customImageLoader,
+        LocalPullToRefresh provides customPullToRefresh,
+        LocalFeeds provides provideFeeds,
+        LocalRestaurantInfo provides CustomRestaurantInfo
     ) {
         RestaurantNavScreen(restaurantId = 234)// 정보 탭, 메뉴 탭, 갤러리 탭, 리뷰 탭 내비게이션
 //        RestaurantMenuScreen(restaurantId = 234)
